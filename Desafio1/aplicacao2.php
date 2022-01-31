d244bb5cb9591a678cdedd67726b6afb464f06e7
Thiago Pereira Gomes <thiago.gomes013@gmail.com>
2022-01-31 15:52:10 -0300

    alerando o nome da função

---
+++ Desafio1/aplicacao2.php
@@ -0,0 +1,21 @@
+<?php
+
+$argumento1 = $argv[1];
+$argumento2 = $argv[2];
+
+
+$pastaOrigem = 'arquivoBase64/';
+$caminho = $pastaOrigem . $argumento1;
+$pastaDestino = 'final/';
+$caminho2 = $pastaDestino . $argumento2;
+
+
+$terra_base_64 = file_get_contents($caminho);
+$conteudo_em_bytes = base64_decode($terra_base_64);
+file_put_contents($caminho2, $conteudo_em_bytes);
+
+//$terra_base_64 = file_get_contents("arquivoBase64/gatinho.txt");
+//$conteudo_em_bytes = base64_decode($terra_base_64);
+//file_put_contents("final/gatinho_4.png", $conteudo_em_bytes);
+
+
